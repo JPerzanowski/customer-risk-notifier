020ad6f1f46d83bf1092949aa9114fe86e57e1cd
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-07-11 11:25:32 +0200

    #4 schedule job risk file

--- src/main/java/com/jakub/recruitment/customerrisknotifier/service/job/CustomerRiskCalculationJob.java
+++ src/main/java/com/jakub/recruitment/customerrisknotifier/service/job/CustomerRiskCalculationJob.java
@@ -4,10 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.*;
-
 @Component
 public class CustomerRiskCalculationJob {
     @Value("${app.file.location}")
@@ -15,27 +11,6 @@ public class CustomerRiskCalculationJob {
 
     @Scheduled(cron = "${app.job.cron-customer-risk-calculation}")
     public void customerRiskCalculationJob() {
-        try {
-            WatchService watchService = FileSystems.getDefault().newWatchService();
-            File uploadDir = new File(uploadFileLocation);
-            if (!uploadDir.exists())
-                Files.createDirectory(uploadDir.toPath());
-            uploadDir.toPath().register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
-                    StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
-            WatchKey key;
-            try {
-                while ((key = watchService.take()) != null) {
-                    key.pollEvents().get(0);
-                    Files.list(Paths.get(uploadFileLocation))
-                            .map(p -> p.getFileName().toFile().getName())
-                            .toList().forEach(System.out::println);
-                    key.reset();
-                }
-            } catch (InterruptedException e) {
-                System.err.println(e.getMessage());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
