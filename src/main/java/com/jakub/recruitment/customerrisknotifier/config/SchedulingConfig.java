dce9ace9270dce23024cb631295c17c769e142ca
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-07-11 11:42:40 +0200

    #5 risk file mapping and feeding

---
+++ src/main/java/com/jakub/recruitment/customerrisknotifier/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.jakub.recruitment.customerrisknotifier.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
